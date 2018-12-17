f4b98e1de0a201ee8c65f45332aa29d54e71ac1c
Harel Bar <harel.games@gmail.com>
2018-12-17 12:20:16 +0200

    TestLibrary code final

--- TestLibrary/TestLibrary.ino
+++ TestLibrary/TestLibrary.ino
@@ -1,12 +1,25 @@
-#include "Controller.h"
-Controller Controller(6,7);
-//Controller* ControllerRef=&Controller;
+#include "Drone.h"
+Drone drone(6,7);
+Drone* droneRef=&drone;
 MessageHeader* message;
 void setup() {
-  //ControllerRef->SetUpTempSensor()->SetUpColorSensor();
+  droneRef->SetUpTempSensor()->SetUpColorSensor();
 }
 
 void loop() {
-  Controller.RecieveMessage(message);
-
+  if(drone.RecieveMessage(message))
+  {
+    if(message->type==REQUEST_FOR_TEMPERATURE)
+    {
+      drone.SendTemperature();
+    }
+    if(message->type==REQUEST_FOR_COLOR)
+    {
+      drone.SendColor();
+    }
+    if(message->type==REQUEST_FOR_HEIGHT)
+    {
+      drone.SendHeight();
+    }
+  }
 }

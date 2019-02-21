125e55fbfd755d29d07c12806c7a50b65ebe346a
Harelon <harel.mspc@gmail.com>
2019-02-21 10:59:49 +0200

    finalized CustomLedLibrary
    Signed-off-by: Harelon <harel.mspc@gmail.com>

--- CustomLed/CustomLed.cpp
+++ CustomLed/CustomLed.cpp
@@ -1,64 +1,123 @@
 #include <CustomLed.h>
 
-CustomLed::CustomLed(char pin)
+CustomLed::CustomLed(int pin)
 {
-    *_pin = pin;
+    _pin = pin;
 }
 
 CustomLed::CustomLed()
 {
-    *_pin = DEFAULT_PIN;
+    _pin = DEFAULT_PIN;
 }
 
 void CustomLed::begin()
 {
-    strip = Adafruit_NeoPixel(16,*_pin, NEO_GRB + NEO_KHZ800);
     #if defined (__AVR_ATtiny85__)
         if (F_CPU == 16000000)
             clock_prescale_set(clock_div_1)
     #endif
-    strip.begin();
+    strip.updateType(NEO_GRB + NEO_KHZ800);
+    strip.updateLength(16);
+    strip.setPin(_pin);
+    strip.begin();    
 }
 
-bool CustomLed::SetLeds(char redValue, char greenValue, char blueValue, char startWing, char startLed, char stopWing, char stopLed)
+bool CustomLed::SetLeds(byte redValue, byte greenValue, byte blueValue, byte startWing, byte startLed, byte stopWing, byte stopLed)
 {
-    char realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
-    char realStoptLed = stopLed - 1  + NUMBER_OF_WINGS * (stopWing - 1);
+    byte realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
+    byte realStoptLed = stopLed  + NUMBER_OF_WINGS * (stopWing - 1);
+    byte NunmberOfLeds = realStoptLed - realStartLed;
     if(realStartLed >= realStoptLed || realStartLed > 15 || realStartLed < 0 || realStoptLed > 15 || realStoptLed < 0)
     {
         return false;
     }
-    for(char i = realStartLed; i <= realStopLed; i++)
-    {
-        strip.setPixelColor(i, redValue, greenValue, blueValue);
-    }
+    strip.fill(Adafruit_NeoPixel::Color(redValue,greenValue,blueValue),realStartLed,NunmberOfLeds);
     strip.show();
     return true;
 }
 
-bool CustomLed::SetLeds(char redValue, char greenValue, char blueValue, char startWing, char startLed, char NunmberOfLeds)
+bool CustomLed::SetLeds(uint32_t color, byte startWing, byte startLed, byte stopWing, byte stopLed)
 {
-    char realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
-    if (realStartLed < 0 || realStartLed < 15)
+    byte realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
+    byte realStoptLed = stopLed  + NUMBER_OF_WINGS * (stopWing - 1);
+    byte NunmberOfLeds = realStoptLed - realStartLed;
+    if(realStartLed >= realStoptLed || realStartLed > 15 || realStartLed < 0 || realStoptLed > 15 || realStoptLed < 0)
     {
         return false;
     }
-    for(char i = realStartLed; i <= realStartLed + NunmberOfLeds; i++)
-    {
-        strip.setPixelColor(i, redValue, greenValue, blueValue);
-    }
+    strip.fill(color, realStartLed, NunmberOfLeds);
     strip.show();
     return true;
 }
 
-bool CustomLed::SetLed(char redValue, char greenValue, char blueValue, char wing, char led)
+bool CustomLed::SetLeds(byte redValue, byte greenValue, byte blueValue, byte startWing, byte startLed, byte NunmberOfLeds)
 {
-    char realLed = led - 1 + NUMBER_OF_WINGS * (wing - 1);
-    if (realLed < 0 || realLed < 15) 
+    byte realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
+    if (realStartLed < 0 || realStartLed > 15)
     {
         return false;
     }
-    strip.setPixelColor(realLed, redValue, greenValue, blueValue);
+    strip.fill(Adafruit_NeoPixel::Color(redValue,greenValue,blueValue),realStartLed,NunmberOfLeds);
+    strip.show();
+    return true;
+}
+
+bool CustomLed::SetLeds(uint32_t color, byte startWing, byte startLed, byte NunmberOfLeds)
+{
+    byte realStartLed = startLed - 1 + NUMBER_OF_WINGS * (startWing - 1);
+    if (realStartLed < 0 || realStartLed > 15)
+    {
+        return false;
+    }
+    strip.fill(color, realStartLed, NunmberOfLeds);
+    strip.show();
+    return true;
+}
+
+bool CustomLed::SetLed(byte redValue, byte greenValue, byte blueValue, byte wing, byte led)
+{
+    byte realLed = led - 1 + NUMBER_OF_WINGS * (wing - 1);
+    if (realLed < 0 || realLed > 15) 
+    {
+        return false;
+    }
+    strip.fill(Adafruit_NeoPixel::Color(redValue,greenValue,blueValue),realLed,1);
+    strip.show();
+    return true;
+}
+
+bool CustomLed::SetLed(uint32_t color, byte wing, byte led)
+{
+    byte realLed = led - 1 + NUMBER_OF_WINGS * (wing - 1);
+    if (realLed < 0 || realLed > 15) 
+    {
+        return false;
+    }
+    strip.fill(color, realLed, 1);
+    strip.show();
+    return true;
+}
+
+bool CustomLed::SetWing(byte redValue, byte greenValue, byte blueValue, byte wing)
+{
+    if (wing < 1 || wing > 4)
+    {
+        return false;
+    }
+    byte startLed = NUMBER_OF_WINGS * (wing - 1);
+    strip.fill(Adafruit_NeoPixel::Color(redValue,greenValue,blueValue),startLed,4);
+    strip.show();
+    return true;
+}
+
+bool CustomLed::SetWing(uint32_t color, byte wing)
+{
+    if (wing < 1 || wing > 4)
+    {
+        return false;
+    }
+    byte startLed = NUMBER_OF_WINGS * (wing - 1);
+    strip.fill(color, startLed, 4);
     strip.show();
     return true;
 }
--- CustomLed/CustomLed.h
+++ CustomLed/CustomLed.h
@@ -5,21 +5,31 @@
 #include <avr/power.h>
 #endif
 
-#define DEFAULT_PIN 5
+#define DEFAULT_PIN 7
 #define NUMBER_OF_LEDS 16
 #define NUMBER_OF_WINGS 4
+#define LED_RED Adafruit_NeoPixel::Color(255,0,0)
+#define LED_GREEN Adafruit_NeoPixel::Color(0,255,0)
+#define LED_BLUE Adafruit_NeoPixel::Color(0,0,255)
+#define LED_ORANGE Adafruit_NeoPixel::Color(255,127,0)
 
 class CustomLed
 {
     private:
-        char *_pin;
+        int _pin;
         Adafruit_NeoPixel strip;
     public:
-        CustomLed(char pin);
+        CustomLed(int pin);
         CustomLed();
         void begin();
-        bool SetLeds(char redValue, char greenValue, char blueValue, char startWing, char startLed, char stopWing, char stopLed);
-        bool SetLed(char redValue, char greenValue, char blueValue, char wing, char led);
+        bool SetLeds(byte redValue, byte greenValue, byte blueValue, byte startWing, byte startLed, byte stopWing, byte stopLed);
+        bool SetLeds(uint32_t color, byte startWing, byte startLed, byte stopWing, byte stopLed);
+        bool SetLeds(byte redValue, byte greenValue, byte blueValue, byte startWing, byte startLed, byte NunmberOfLeds);
+        bool SetLeds(uint32_t color, byte startWing, byte startLed, byte NunmberOfLeds);
+        bool SetLed(byte redValue, byte greenValue, byte blueValue, byte wing, byte led);
+        bool SetLed(uint32_t color, byte wing, byte led);
+        bool SetWing(byte redValue, byte greenValue, byte blueValue, byte wing);
+        bool SetWing(uint32_t color, byte wing);
         void TurnOffLed();
 };
 

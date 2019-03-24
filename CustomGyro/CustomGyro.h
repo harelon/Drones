1ed5d88c4de036a34a042333ec491725233430fd
Harelon <harel.mspc@gmail.com>
2019-03-24 11:13:50 +0200

    changed Gyro to work with a new mpu6050 library
    Signed-off-by: Harelon <harel.mspc@gmail.com>

--- CustomGyro/CustomGyro.h
+++ CustomGyro/CustomGyro.h
@@ -2,28 +2,23 @@
 #define CustomGyro_h
 
 #include <MPU6050.h>
-#include <math.h>
 
-
-#define GyroScale 131
+#define timeStep 0.2
 class CustomGyro
 {
     private:
         MPU6050 _Gyro;
-        int16_t ax;
-        int16_t ay;
-        int16_t az;
-        int16_t gx;
-        int16_t gy;
-        int16_t gz;
-        float _pitchError;
-        float _rollError;
+        float _pitch;
+        float _roll;
+        float _yaw;
+        long _startTime;
     public:
         CustomGyro();
         void begin();
         float GetPitch();
         float GetRoll();
         float GetYaw();
-        void ResetPitchAndRoll();
+        void startTime();
+        void updateValues();
 };
 #endif

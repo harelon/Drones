71db908de42a9f32e3be5d9a363db2a5b826b185
Harelon <harel.games@gmail.com>
2019-03-28 09:20:40 +0200

    added ServoBall library with example that works
    Signed-off-by: Harelon <harel.games@gmail.com>

---
+++ ServoBall/ServoBall.cpp
@@ -0,0 +1,46 @@
+#include <ServoBall.h>
+
+ServoBall::ServoBall()
+{
+    _pin = -1;
+    _color = -1;
+    _state = NOT_LOADED;
+}
+
+void ServoBall::Init(byte pin, ServoColors color)
+{
+    Init(pin, color, NEED_TO_LOAD);
+}
+
+void ServoBall::Init(byte pin, ServoColors color, BallStates state)
+{
+    _pin = pin;
+    _color = color;
+    _state = state;
+    _servo.attach(_pin);
+}
+
+BallStates ServoBall::Lock()
+{
+    if(_state == NEED_TO_LOAD)
+    {
+        _servo.write(30);
+        _state = LOADED;
+    }
+    return GetState();
+}
+
+BallStates ServoBall::Drop()
+{
+    if(_state == LOADED)
+    {
+        _servo.write(110);
+        _state = DROPPED;
+    }
+    return GetState();
+}
+
+BallStates ServoBall::GetState()
+{
+    return _state;
+}

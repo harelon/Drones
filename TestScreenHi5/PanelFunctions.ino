5a1e43c053ec797cc09b12a50d8cee1d7e2a49b7
Yehonatan Rafael Boltansky <boltanskyyehonatan@gmail.com>
2019-03-29 14:29:39 +0300

    Screen Lib requests data onResponses need work
    Signed-off-by: Yehonatan Rafael Boltansky <boltanskyyehonatan@gmail.com>

--- TestScreenHi5/PanelFunctions.ino
+++ TestScreenHi5/PanelFunctions.ino
@@ -3,13 +3,12 @@ void PrintStatus(Adafruit_ILI9341* lcd, char* s)
   lcd->fillRect(1, 226, 238, 43, ILI9341_BLACK);
   lcd->setCursor(12, 237);
   lcd->setTextColor(ILI9341_YELLOW);
-  lcd->println(s);
+  lcd->print(s);
 }
 
-void PrintResult(Adafruit_ILI9341* lcd, char* s)
+void PrintResult(Adafruit_ILI9341* lcd)
 {
   lcd->fillRect(1, 272, 238, 43, ILI9341_BLACK);
   lcd->setCursor(12, 286);
   lcd->setTextColor(ILI9341_GREEN);
-  lcd->println(s);
 }

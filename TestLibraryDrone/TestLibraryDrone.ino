cd1b9a8aa0e7296ae5c0561af2d9a031822d21f4
Harelon <harel.games@gmail.com>
2019-04-01 12:53:55 +0300

    test drone works with all sensors
    Signed-off-by: Harelon <harel.games@gmail.com>

--- TestLibraryDrone/TestLibraryDrone.ino
+++ TestLibraryDrone/TestLibraryDrone.ino
@@ -3,8 +3,11 @@ Drone drone(5, 4);
 Drone* droneRef = &drone;
 void setup() {
   Serial.begin(9600);
-  droneRef->SetUpColorSensor()->SetUpHeightSensor(8);
+  droneRef->SetUpColorSensor()->SetUpHeightSensor(8)->SetUpGyro();
+  droneRef->SetUpServo(9,SERVO_RED)->SetUpServo(10,SERVO_GREEN)->SetUpServo(11,SERVO_BLUE);
 }
 void loop() {
-  drone.PollMessage();  
+  drone._Gyro.startTime();
+  drone.PollMessage();
+  drone._Gyro.updateValues();
 }

3a314b041feb1fb51a8378c99befb79907c39234
Harelon <harel.mspc@gmail.com>
2019-04-01 15:54:39 +0300

    moved data structures to utils
    Signed-off-by: Harelon <harel.mspc@gmail.com>

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -9,7 +9,33 @@ typedef enum {
   MAIN_GREEN =  0x00FF00,
   MAIN_BLUE =   0x0000FF,
   MAIN_ WHITE = 0xFFFFFF,
+  MAIN_INTERMEDIATE = 0x000001,
 } MainColors;
+
+typedef enum {
+    SERVO_RED = 0,
+    SERVO_GREEN = 1,
+    SERVO_BLUE = 2,
+} ServoColors;
+
+typedef enum{
+    NOT_LOADED = 0,
+    NEED_TO_LOAD = 1,
+    LOADED = 2,
+    DROPPED = 3,
+} BallStates;
+
+typedef struct {
+  uint16_t r;
+  uint16_t g;
+  uint16_t b;
+} rgbColor;
+
+typedef struct{
+   int16_t pitch;
+   int16_t roll;
+   int16_t yaw;
+} AngularOrientation;
 
 class Utils
 {

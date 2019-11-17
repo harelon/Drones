1131059e1f93b5706ae5c87037f02ded3d9c744e
Harel Bar <harel.games@gmail.com>
2019-11-17 11:07:28 +0200

    started drones app
    Signed-off-by: Harel Bar <harel.games@gmail.com>

---
+++ DroneScreen/app/src/main/java/com/example/dronescreen/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dronescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothHeadset;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] permissions = new String[]{Manifest.permission.BLUETOOTH};
+        ActivityCompat.requestPermissions(MainActivity.this,
+                permissions,
+                1);
+//        if()
+    }
+
+    public static boolean isBluetoothHeadsetConnected() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return mBluetoothAdapter != null && mBluetoothAdapter.isEnabled() && mBluetoothAdapter.getProfileConnectionState(BluetoothHeadset.HEADSET) == BluetoothHeadset.STATE_CONNECTED;
+    }
+}
